a6966b5f51d4b5106e9bfe3d30a37b637c65843a
HaKkaz <luckyanthonyan@gmail.com>
2023-09-28 20:17:38 +0800

    add gitignore and update hw1.cpp

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -12,45 +12,51 @@
 using namespace std;
 
 // get command from stdin
-vector<string> get_command() {
+tuple< vector<string>, bool> get_command() {
     vector<string> cmd;
     string str;
+    bool is_redirection = false;
     getline(cin, str);
     stringstream ss(str);
     while (ss >> str) {
         cmd.push_back(str);
+        if (str == ">") is_redirection = true;
     }
-    return cmd;
+    return make_tuple(cmd, is_redirection);
 }
-
-// set of background process pids
-set< pid_t > pids;
 
 // Signal handler for SIGCHLD
 void sigchld_handler(int signo) {
     int status;
     pid_t pid;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        // Remove the terminated process from the pids set
-        pids.erase(pid);
-    }
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0);
 }
 
-
+// IO redirection
 int main() {
     // Register the SIGCHLD handler
     signal(SIGCHLD, sigchld_handler);
 
     while (true) {
         cout << '>' << flush;
-        auto cmd = get_command();
+        auto [cmd, is_redirection] = get_command();
 
-        // get args and convert to char**
+        // check the child process should run in background or not
         bool is_background = false;
         if (cmd.size() > 1 && cmd[cmd.size() - 1] == "&") {
             is_background = true;
             cmd.pop_back();
         }
+
+        // if the command have io redirection, open the file
+        string refilename = "";
+        if (is_redirection) {
+            refilename = cmd[cmd.size() - 1];
+            cmd.pop_back();
+            cmd.pop_back();
+        }
+
+        // get args and convert to char** 
         char** args = new char*[cmd.size() + 1];
         for (int i = 0; i < cmd.size(); i++) {
             args[i] = (char*)cmd[i].c_str();
@@ -63,18 +69,19 @@ int main() {
             exit(-1);
         }
         else if (pid == 0) {
+            FILE *fp = NULL;
+            if (is_redirection)
+                fp = freopen(refilename.c_str(), "w", stdout);
             execvp(args[0], args);
+            if (is_redirection)
+                fclose(fp);
             exit(1);
         }
-        else {
-            if (!is_background) { 
-                // if not background, wait for child process
-                waitpid(pid, NULL, 0);
-            } else { 
-                // if background, add pid to set
-                pids.emplace(pid);
-            }
+        else if (!is_background) { 
+            // if not background, wait for child process
+            waitpid(pid, NULL, 0);
         }
+
         delete[] args;
     }
 }
